1261b184498f57614438801829d7a32a26047666
NitroFuN <theedge5670@gmail.com>
2020-08-03 22:08:15 +0200

    kensurbot: Move to my repo

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -76,7 +76,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 
 # Custom (forked) repo URL and BRANCH for updater.
 UPSTREAM_REPO_URL = os.environ.get("UPSTREAM_REPO_URL",
-                                   "https://github.com/KenHV/KensurBot.git")
+                                   "https://github.com/NitroFuN/KensurBot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "sql-extended")
 
 # Console verbose logging
